cae64551e33d6d20a3c39a73dece0eeec081d0b6
rkaruna1 <rknidhi@gmail.com>
2018-01-21 21:21:08 -0500

    My first shellProg commiting

---
+++ firstshellProg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Sheel Env : Sh bash csh tcsh ksh 
+# Name of the Script: disk_usage.sh 
+# 
+#Author: karuna Raj
+#Date  : 01/21/2018
+    echo "This is my First Program"
+
+    Today_Date=`date +%y-%m-%d`
+    echo "We are calling a variable"
+    echo $Today_Date
+    echo "Now, we are calling the fucntion"
+    
